8a75236c7aa4d0a20d415fe214f583f5bcaf55b4
brian@palominodb.com <brian>
2009-08-10 03:53:58 +0000

    Make deploy.sh less noisy.

--- deploy.sh
+++ deploy.sh
@@ -11,9 +11,9 @@ fi
 HOSTS="testdb1 testdb2"
 for host in $HOSTS
 do
-	rsync -avzP $code_base/ root@$host:/usr/share/mysql-zrm/plugins/
-	ssh root@$host 'chown -vR mysql:mysql /usr/share/mysql-zrm/plugins'
-	ssh root@$host 'chown -vR mysql:mysql /etc/mysql-zrm'
-	ssh root@$host 'mkdir -vp /mysqlbackups && chown -vR mysql:mysql /mysqlbackups'
-	ssh root@$host 'chown -vR mysql:mysql /var/log/mysql-zrm'
+	rsync -avP $code_base/ root@$host:/usr/share/mysql-zrm/plugins/
+	ssh root@$host 'chown -R mysql:mysql /usr/share/mysql-zrm/plugins'
+	ssh root@$host 'chown -R mysql:mysql /etc/mysql-zrm'
+	ssh root@$host 'mkdir -p /mysqlbackups && chown -R mysql:mysql /mysqlbackups'
+	ssh root@$host 'chown -R mysql:mysql /var/log/mysql-zrm'
 done
