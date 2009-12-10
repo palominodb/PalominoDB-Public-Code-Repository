b6ad70453f8d3599074666615b312904ccfb1928
brian@palominodb.com <brian>
2009-12-10 23:18:32 +0000

    Make history and database pages more robust.
    These pages would occasionally throw nil errors if "bad"
    data was given to them. Make this not the case.

--- tools/ttt_gui/app/models/database.rb
+++ tools/ttt_gui/app/models/database.rb
@@ -3,8 +3,6 @@ require 'ttt/collector'
 require 'ttt/server'
 require 'set'
 class Database
-  attr_reader :server
-  attr_reader :name
   attr_reader :stats
   def self.find(server,name)
     db=TTT::Server.find_by_name(server).schemas.find_by_name(name)
@@ -14,25 +12,21 @@ class Database
         stats[s]=k.find_most_recent_versions({:conditions => ['server = ? and database_name = ?', db.server.name, db.name]})
         #stats[s]=k.find(:all, :conditions => ["server = ? and database_name = ? and run_time = ?", server, name, TTT::Collector.get_last_run(s)])
       end
-      self.new(server,name, stats)
+      self.new(db, stats)
     else
       nil
     end
   end
   def self.all
     TTT::Schema.all(:order => 'cached_size DESC').map { |s| self.find(s.server.name, s.name) }
-    #dbs=Set.new
-    ##TTT::TrackingTable.tables.each do |s,k|
-    ##  stats[s]=k.find(:all, :conditions => ["server = ? and database_name = ? and run_time = ?", server, name, TTT::Collector.get_last_run(s)])
-    ##end
+  end
 
-    #TTT::TrackingTable.tables.each do |s,tt|
-    #  dbs.merge tt.find(:all, :select => "server, database_name", :group => 'server, database_name', :conditions => ["run_time = ?", TTT::Collector.get_last_run(s)]).map { |s|
-    #    self.find(s.server,s.database_name)
-    #  }
-    #end
+  def server
+    @db.server
+  end
 
-    #dbs
+  def name
+    @db.name
   end
 
   def hash
@@ -45,7 +39,7 @@ class Database
   end
 
   def get_size
-    Database.get_size(server,name)
+    @db.cached_size
     #size=0
     #@stats[:volume].each do |d|
     #  size+=d.size
@@ -60,7 +54,7 @@ class Database
   end
 
   def tables
-    TTT::Server.find_by_name(server).schemas.find_by_name(name).tables
+    @db.tables
   end
 
   #def tables
@@ -72,9 +66,8 @@ class Database
   #  tbls.collect { |t| Table.find(@server, @name, t) }
   #end
 
-  def initialize(server,db,stats)
-    @server=server
-    @name=db
+  def initialize(db,stats)
+    @db=db
     @stats=stats
   end
 end
--- tools/ttt_gui/app/models/table.rb
+++ tools/ttt_gui/app/models/table.rb
@@ -10,7 +10,6 @@ class Table
   def self.find(server,db,name)
     stats={}
     TTT::TrackingTable.tables.each do |s,k|
-      #stats[s]=k.find(:last, :conditions => ["server = ? and database_name = ? and table_name = ? and run_time = ?", server, db, name, TTT::Collector.get_last_run(s)])
       stats[s]=k.find(:last, :conditions => ["server = ? and database_name = ? and table_name = ?", server, db, name])
     end
     self.new(server,db,name,stats)
@@ -19,7 +18,6 @@ class Table
   def self.find_at(server,db,name,at)
     stats={}
     TTT::TrackingTable.tables.each do |s,k|
-      #stats[s]=k.find(:last, :conditions => ["server = ? and database_name = ? and table_name = ? and run_time = ?", server, db, name, TTT::Collector.get_last_run(s)])
       stats[s]=k.find(:last, :conditions => ["server = ? and database_name = ? and table_name = ? and run_time = ?", server, db, name, Time.at(at.to_i)])
     end
     self.new(server,db,name,stats)
@@ -27,6 +25,10 @@ class Table
 
   def get_volume
     @stats[:volume]
+  end
+
+  def stats?
+    @stats[:definition] || @stats[:view] || @stats[:volume]
   end
 
   def size
@@ -58,14 +60,20 @@ class Table
   end
 
   def get_create
-    table_type == :base ? @stats[:definition].create_syntax : @stats[:view].create_syntax
+    if table_type == :base
+      @stats[:definition].create_syntax
+    elsif table_type == :view
+      @stats[:view].create_syntax
+    else
+      nil
+    end
   end
 
   def table_type
     type=:base
     if @stats[:definition].nil? and @stats[:view].nil?
       type=:unknown
-    elsif @stats[:definition].nil? and !@stats[:view].nil?
+    elsif @stats[:definition].nil? and @stats[:view]
       type=:view
     end
     type
