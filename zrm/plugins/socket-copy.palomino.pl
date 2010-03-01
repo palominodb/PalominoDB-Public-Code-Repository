baf1403a50bf2228e688a8d0009edd02b829efaf
brian@palominodb.com <brian>
2010-03-01 20:42:20 +0000

    Additional work on applying xtrabackup logs.

--- zrm/plugins/socket-copy.palomino.pl
+++ zrm/plugins/socket-copy.palomino.pl
@@ -426,7 +426,13 @@ sub readInnoBackupStream()
 
   if( $config{'apply-xtrabackup-logs'} == 1 ) {
     $pl->m("Applying logs..");
-    $pl->x(\&system, "innobackup-1.5.1 --apply-log $destDir");
+    my %r = $pl->x(sub { system @_; }, "cd $destDir && innobackupex-1.5.1 --apply-log $destDir");
+    my $fh = $r{fh};
+    while(<$fh>) { $pl->m($_); }
+    if($r{rcode} != 0) {
+      $pl->i("Applying the innobackup logs failed.");
+    }
+    if($r{error}) { &printAndDie("Error executing innobackupex."); }
   }
 }
 
