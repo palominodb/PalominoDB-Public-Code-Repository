2228da1cea018b86520220a77d3ce9be20d92854
brian@palominodb.com <brian>
2010-04-06 05:30:27 +0000

    Import puppet changes from chegg.

---
+++ puppet/modules/mysql/manifests/user.pp
@@ -0,0 +1,36 @@
+class mysql::user {
+  $homedir = $mysql_homedir ? {
+    '' => "/home/mysql",
+    default => $mysql_homedir,
+  }
+  $shell = $mysql_usershell ? {
+    '' => $operatingsystem ? {
+      "freebsd" => "/usr/local/bin/bash",
+      "centos"  => "/bin/bash",
+      "redhat"  => "/bin/bash",
+      "ubuntu"  => "/bin/bash",
+      "debian"  => "/bin/bash",
+      },
+    default => $mysql_usershell
+  }
+  user { "mysql":
+    uid => '88',
+    gid => '88',
+    home => $homedir,
+    ensure => 'present',
+    shell  => $shell,
+  }
+
+  file { "$homedir/.ssh":
+    ensure => 'directory',
+    owner => 'mysql',
+    group => 'mysql',
+    mode  => '0700',
+  }
+
+  ssh_authorized_key{ 'slow_query_profiler@ops':
+    key => 'AAAAB3NzaC1yc2EAAAABIwAAAIEAvFdoxqjLJBAHkCstFGTvKeEonGUOS80XqSTzpflk24GGISDIHhTkU+JUFFcqU8Plji1fgufpVZYltiOE/C0zFWI1GJNi4xbVjwc3Ez2YWG+8aAKxyJ4KfFbjpZIb95zi7NHSu5hAqBJtH6HQWUZtrE3OXbDyexTqPp0gaOncvuk=',
+    type => 'ssh-rsa',
+    user => 'mysql',
+  }
+}
