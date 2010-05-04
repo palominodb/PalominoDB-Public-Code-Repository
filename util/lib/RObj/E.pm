a9ac2323193bd9e6ede6ceff7882a66c9cec497d
brian@palominodb.com <brian>
2010-05-04 00:43:43 +0000

    RObj's now catch die() and send a death message

--- util/lib/RObj/E.pm
+++ util/lib/RObj/E.pm
@@ -1,33 +1,5 @@
-# Copyright (c) 2009-2010, PalominoDB, Inc.
-# All rights reserved.
-# 
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-# 
-#   * Redistributions of source code must retain the above copyright notice,
-#     this list of conditions and the following disclaimer.
-# 
-#   * Redistributions in binary form must reproduce the above copyright notice,
-#     this list of conditions and the following disclaimer in the documentation
-#     and/or other materials provided with the distribution.
-# 
-#   * Neither the name of PalominoDB, Inc. nor the names of its contributors
-#     may be used to endorse or promote products derived from this software
-#     without specific prior written permission.
-# 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
 # ###########################################################################
-# RObj::Base package 2052bc8fe38f08c660f2fc8d830f2491eda1b226
+# RObj::Base package 7b005acbf29f2c5bb30272a197ead7bdf4977ce6
 # ###########################################################################
 package RObj::Base;
 use strict;
@@ -49,6 +21,7 @@ use vars qw(@ISA $VERSION @EXPORT);
 
 $VERSION = 0.01;
 
+use constant NATIVE_DEATH      => -3;
 use constant COMPILE_FAILURE   => -2;
 use constant TRANSPORT_FAILURE => -1;
 use constant OK => 0;
@@ -152,7 +125,7 @@ RObj::Base->import;
 
 use constant COMPILE_FAILURE => RObj::Base::COMPILE_FAILURE;
 use constant TRANSPORT_FAILURE => RObj::Base::TRANSPORT_FAILURE;
-use constant NATIVE_DEATH => -3;
+use constant NATIVE_DEATH => RObj::Base::NATIVE_DEATH;
 use constant OK => RObj::Base::OK;
 
 my $ro = RObj::Base->new;
