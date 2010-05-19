4937f575013080db7194c770aeb611ddbb481d22
brian@palominodb.com <brian>
2010-05-19 18:47:44 +0000

    Support checksumming specific tables
    - No longer depend on some table having checksum differences
      in order to capture results - walk the slaves ourself.
    - Document YAML DSN keys that this tool uses
    - Support specifying logpath - enables syslog logging
    - Support overriding DSN on commandline for some parameters

--- nagios/table_checksums/pdb-dsn-checksum.in.pl
+++ nagios/table_checksums/pdb-dsn-checksum.in.pl
@@ -22,7 +22,7 @@ use strict;
 use warnings FATAL => 'all';
 use English qw(-no_match_vars);
 use Data::Dumper;
-use Getopt::Long;
+use Getopt::Long qw(:config no_ignore_case);
 use Pod::Usage;
 use List::Util qw(max);
 
@@ -49,8 +49,11 @@ my %checksum_master_opts;
 my $csum_dsn = undef;
 my $csum_dbh = undef;
 
-my $global_chunk_size = 50_000;
+my $default_chunk_size = 50_000;
 my $cluster = undef;
+
+my $cli_chunk_size = 0;
+my $cli_since = '';
 
 sub main {
   my (@ARGV) = @_;
@@ -58,15 +61,17 @@ sub main {
     'help|?'  => sub { pod2usage(-verbose => 99); },
     'dsn|d=s' => \$dsnuri,
     'user|u=s' => \$user,
-    'log=s'    => \$loguri,
+    'logfile|L=s'    => \$loguri,
     'password|p=s' => \$password,
     'store|S=s' => \$central_dsn,
     'replicate-table|R=s' => \$repl_table,
     'only-report' => \$only_report,
     'pretend' => \$pretend,
     'mk-table-checksum-path=s' => \$mk_table_checksum_path,
-    'cluster=s' => \$cluster
-  );
+    'cluster=s' => \$cluster,
+    'chunk-size=s' => \$cli_chunk_size,
+    'since=s' => \$cli_since
+  ) or die('Try --help');
 
   unless(defined($central_dsn) and defined($dsnuri) and defined($user) and defined($password) and defined($repl_table)) {
     pod2usage(-message => '--dsn, --user, --password, --store, and --replicate-table are all required.', -verbose => 99);
@@ -76,15 +81,12 @@ sub main {
   $pl = ProcessLog->new($0, $loguri, undef);
   $pl->start;
 
+  $ENV{MKDEBUG} = exists $ENV{MKDEBUG} ? $ENV{MKDEBUG} : $pretend;
   eval "require '$mk_table_checksum_path'";
   if($EVAL_ERROR) {
     $pl->e($EVAL_ERROR);
     return 1;
   }
-  no warnings;
-  *mk_table_checksum::print_inconsistent_tbls = \&pdb_dsn_checksum::save_to_central_server;
-  use warnings FATAL => 'all';
-
 
   my $dsn = Pdb::DSN->new($dsnuri);
   my $dp = DSNParser->new({key => 't', 'desc' => 'Table to write to', copy => 0});
@@ -108,24 +110,89 @@ sub main {
       if($opts->{chunk_size}) {
         $checksum_master_opts{$s}{chunk_size} = $opts->{chunk_size};
       }
+      if($opts->{tables}) {
+        $checksum_master_opts{$s}{tables} = $opts->{tables};
+      }
+      if($opts->{since}) {
+        $checksum_master_opts{$s}{since} = $opts->{since};
+      }
     }
   }
 
   unless ($only_report) {
     foreach my $cm (@checksum_masters) {
       $pl->i("CHECKSUMMING CLUSTER PRIMARY:",$cm);
-      my @ignore_databases = (@{$checksum_master_opts{$cm}{ignore_databases}}, @global_ignore_databases);
-      my @ignore_tables    = (@{$checksum_master_opts{$cm}{ignore_tables}}, @global_ignore_tables);
-      my $c_size           = $checksum_master_opts{$cm}{chunk_size} || $global_chunk_size;
-      my @mk_args = ('--create-replicate-table', '--empty-replicate-table', '--replicate', $repl_table, '--user', $user, '--password', $password, '--ignore-databases', join(',', @ignore_databases), '--ignore-tables', join(',',@ignore_tables), '--chunk-size', $c_size, $cm);
-      run_mk_checksum(@mk_args);
+      my @ignore_databases = (@{$checksum_master_opts{$cm}{ignore_databases} || []}, @global_ignore_databases);
+      my @ignore_tables    = (@{$checksum_master_opts{$cm}{ignore_tables} || []}, @global_ignore_tables);
+      my @do_tables        = @{$checksum_master_opts{$cm}{tables} || []};
+      my $c_size           = $cli_chunk_size || $checksum_master_opts{$cm}{chunk_size} || $default_chunk_size;
+      my $since            = $cli_since || $checksum_master_opts{$cm}{since};
+
+      # Run through individual tables if 'tables:'
+      # key exists
+      if(scalar @do_tables) {
+        my %w_vals = ();
+        foreach my $t (@do_tables) {
+          foreach my $k (keys %$t) {
+            $w_vals{$t->{$k}} ||= [];
+            push @{$w_vals{$t->{$k}}}, $k;
+          }
+        }
+        foreach my $w (keys %w_vals) {
+          my @tbls = @{$w_vals{$w}};
+          my @mk_args = (
+            $pretend ? ('--explain') : (),
+            !$ENV{Pdb_DEBUG} ? ('--quiet') : (),
+            '--create-replicate-table',
+            '--empty-replicate-table',
+            '--replicate', $repl_table,
+            '--user', $user,
+            '--password', $password,
+            scalar @ignore_databases ? ('--ignore-databases', join(',', @ignore_databases)) : (),
+            scalar @ignore_tables ? ('--ignore-tables', join(',',@ignore_tables)) : (),
+            scalar @do_tables ? ('--tables', join(',',@tbls)) : (),
+            $since ? ('--since', $since) : (),
+            '--chunk-size', $c_size,
+            '--where', $w,
+            $cm
+          );
+          run_mk_checksum(@mk_args);
+        }
+      }
+      else {
+          my @mk_args = (
+            $pretend ? ('--explain') : (),
+            !$ENV{Pdb_DEBUG} ? ('--quiet') : (),
+            '--create-replicate-table',
+            '--empty-replicate-table',
+            '--replicate', $repl_table,
+            '--user', $user,
+            '--password', $password,
+            scalar @ignore_databases ? ('--ignore-databases', join(',', @ignore_databases)) : (),
+            scalar @ignore_tables ? ('--ignore-tables', join(',',@ignore_tables)) : (),
+            $since ? ('--since', $since) : (),
+            '--chunk-size', $c_size,
+            $cm
+          );
+          run_mk_checksum(@mk_args);
+      }
     }
   }
 
   foreach my $cm (@checksum_masters) {
+    my $ms = MasterSlave->new();
+    my $master_dsn = $dp->parse("h=$cm,u=$user,p=$password");
+    my $master_dbh = $dp->get_dbh($dp->get_cxn_params($master_dsn));
     $pl->i("RETRIEVING CLUSTER CHECKSUMS:", $cm);
-    my @mk_args = ('--replicate-check', 2, '--replicate', $repl_table, '--user', $user, '--password', $password, $cm);
-    run_mk_checksum(@mk_args);
+    $ms->recurse_to_slaves(
+      {
+        dbh  => $master_dbh,
+        dsn  => $master_dsn,
+        dsn_parser => $dp,
+        callback => \&save_to_central_server
+      }
+    );
+    $master_dbh->disconnect;
   }
   $csum_dbh->commit;
 
@@ -171,23 +238,17 @@ sub run_mk_checksum {
     local $INPUT_RECORD_SEPARATOR;
     $pl->d(<$fh>);
   }
-  if($r->{error}) {
-    $pl->es("Error calling mk-table-checksum:", $r->{error});
+  if($r->{rcode}) {
+    $pl->e("Error calling mk-table-checksum:", 'exit:', $r->{rcode}, $r->{error});
   }
 }
 
 sub save_to_central_server {
-  my (%args) = @_;
-  foreach my $arg ( qw(o dp dsn tbls) ) {
-    die "I need $arg" unless $args{$arg};
-  }
+  my ( $dsn, $dbh, $level, $parent ) = @_;
   my $central_table = "`$csum_dsn->{D}`.`$csum_dsn->{t}`";
-  my $dbh = $args{dbh};
-  my $dsn = $args{dsn};
-  my $o   = $args{o};
   my $host = $dbh->quote($dsn->{h});
-  my $del_sql = qq# DELETE FROM $central_table WHERE host=$host #;
-  print(STDERR 'SQL: ', $del_sql);
+  my $del_sql = qq#DELETE FROM $central_table WHERE host=$host AND ts < NOW() - INTERVAL 1 WEEK#;
+  $pl->d('SQL:', $del_sql);
   RETRY:
   eval {
     $csum_dbh->do($del_sql);
@@ -207,18 +268,18 @@ sub save_to_central_server {
         master_crc char(40)         NULL,
         master_cnt int              NULL,
         ts         timestamp    NOT NULL,
-        PRIMARY KEY (host, db, tbl, chunk)
+        PRIMARY KEY (host, db, tbl, chunk, ts)
       ) ENGINE=InnoDB
     EOF
-    print(STDERR 'SQL: ', $creat_sql);
+    $pl->d('SQL:', $creat_sql);
     $csum_dbh->do($creat_sql);
     goto RETRY;
   }
-  foreach my $r ( @{$dbh->selectall_arrayref('SELECT * FROM '. $args{o}->get('replicate'), { Slice => {} })} ) {
+  foreach my $r ( @{$dbh->selectall_arrayref('SELECT * FROM '. $repl_table, { Slice => {} })} ) {
     my $magic_cols = join(',', map { $_ } sort keys %$r);
     my $magic_vals = join(',', map { $dbh->quote($r->{$_}) } sort keys %$r);
-    my $ins_sql = qq# INSERT INTO $central_table (host,$magic_cols) VALUES (${host},$magic_vals) #;
-    print(STDERR 'SQL: ', $ins_sql);
+    my $ins_sql = qq#INSERT INTO $central_table (host,$magic_cols) VALUES (${host},$magic_vals) #;
+    $pl->d('SQL:', $ins_sql);
     $csum_dbh->do($ins_sql);
   }
   return;
@@ -237,6 +298,13 @@ pdb-dsn-checksum - Checksum servers using PalominoDB DSN as source
 pdb-dsn-checksum -d <uri> -s <dsn> -R <db.table> -u <user> -p <pw>
 
 All options mentioned above are required for operation.
+
+=head1 EXAMPLES
+
+  pdb-dsn-checksum --dsn /tmp/dsn.yml \
+    --store h=ops,u=ops,p=ops,D=checksums,t=checksums \
+    --replicate-table checksum.checksum --user csum \
+    --password csum --pretend
 
 
 =head1 OPTIONS
@@ -263,7 +331,7 @@ Path to mk-table-checksum. Required to function as this tool is merely a wrapper
 
 =item --dsn,-d <uri>
 
-PalominoDB DSN URI.
+PalominoDB YAML DSN.
 
 =item --store,-s <dsn>
 
@@ -271,6 +339,10 @@ Maatkit-style DSN pointing to table (central checksum result storage).
 Example:
 
   h=testdb1,u=root,p=pass,D=test_db,t=checksum_table
+
+At present, this table is created automatically for you, and,
+results older than 1 week are purged when updating it.
+There is not currently a way to configure that.
 
 =item --replicate-table,-R <db.tbl>
 
@@ -287,6 +359,29 @@ Select a single cluster out of many enabled for checksumming in a dsn.
 Normally, the behavior of this program is to checksum each cluster
 listed as enabled in serial. This option negates that.
 
+=item --chunk-size
+
+Override the default chunk size of 50k rows, or, whatever is set
+in the DSN.
+
+=item --since
+
+B<THIS DOES NOT DO WHAT YOU WANT. REALLY.>
+
+Only checksum rows since some MySQL expression. See the
+mk-table-checksum docs as this value is passed verbatim
+onto that tool.
+
+mk-table-checksum does some kind of wacky thing where it assumes
+that if the expression given here evaluates to a timestamp,
+then, the WHOLE TABLE should be checksumed if it has data
+newer than that value. If it's not a timestamp, (i.e., a number),
+then it'll checksum rows with values larger than that number.
+Dumb, right?
+
+What you really want is to setup the YAML DSN so that it contains
+the appropriate where clauses. See below for how to do that.
+
 =item --user,-u <user>
 
 Username for all servers.
@@ -295,12 +390,81 @@ Username for all servers.
 
 Password for user.
 
-=head1 EXAMPLES
+=item --logfile,-L <path>
 
-  pdb-dsn-checksum --dsn /tmp/dsn.yml \
-    --store h=ops,u=ops,p=ops,D=checksums,t=checksums \
-    --replicate-table checksum.checksum --user csum \
-    --password csum --pretend
+Where to log debugging and informational messages.
+Like all other PDB tools, you can also specify syslog:<facility>
+to log to syslog.
+
+Default: ./pdb-dsn-checksum.log
+
+=back
+
+=head1 YAML DSN KEYS
+
+This tool recognizes the following keys in a YAML DSN:
+These all go on primary hosts, not clusters.
+
+=over 4
+=item checksum
+
+type: boolean (y/n)
+
+If 'n', then this tool will skip this cluster.
+
+=item checksum_options:
+
+type: group
+
+Holds all options for the checksum tool.
+
+Keys:
+
+=over 4
+
+=item tables
+
+type: array
+
+A list/array of tables (qualified with database name) to checksum.
+Each item should be the key to a one element hash where the value
+is the where clause to apply to that table.
+
+Example:
+
+  checksum_options:
+    tables:
+    - test.test_data: "ts > NOW() - INTERVAL 1 HOUR"
+
+=item since
+
+type: expression
+
+This is the same as the L<--since> flag. Most likely you don't want this.
+
+=item ignore_tables
+
+type: array
+
+Tables (qualified with database name) to ignore when checksumming, if L<tables>
+wasn't specified.
+
+=item ignore_databases
+
+Databases to ignore (skip) when checksumming.
+
+=item chunk_size
+
+How large the chunks should be for checksumming.
+Smaller chunks generally mean a slightly longer wait for results, but
+less lock contention.
+
+Default: 50,000
+
+=back
+
+=back
+
 
 =cut
 
