aadb27cd6b3dd20301067c3e12c227e7effbf5f3
brian@palominodb.com <brian>
2010-12-12 21:34:09 +0000

    Add agebyname predicate to TableFind, fix broken AND-ing logic in the predicate testing. Add regression test for that bug, and new tests for agebyname predicate usage.

--- util/lib/TableFind.pm
+++ util/lib/TableFind.pm
@@ -31,45 +31,52 @@ use strict;
 use warnings FATAL => 'all';
 use Carp;
 use DateTime;
-use TableAge;
+use DateTime::Format::Strptime;
 use DSN;
 
 sub new {
-	my ($class, $dsn) = @_;
-	my $self = {};
-	croak("Need D key") unless($dsn->get('D'));
-	$self->{dsn} = $dsn;
-	return bless $self, $class;
+  my ($class, $dsn) = @_;
+  my $self = {};
+  croak("Need D key") unless($dsn->get('D'));
+  $self->{dsn} = $dsn;
+  return bless $self, $class;
 }
 
 sub find {
-	my ($self, @preds) = @_;
-	my @res;
-	my $tbl_stat = $self->{dsn}->get_dbh(1)->selectall_arrayref(
-	       "SHOW TABLE STATUS FROM `". $$self{dsn}->get('D') ."`",
-	       { Slice => {} }
-	   );
-	foreach my $tbl (@$tbl_stat) {
-  	for(my $i=0; $i<$#preds; $i++) {
-	  	no strict 'refs';
-		  my $pred = $preds[$i];
-		  my $pred_args = $preds[$i+1];
-		  eval {
-  		  if(ref($pred_args) eq 'ARRAY') {
-  		    &{"PREDICATE_$pred"}($self, $tbl, @$pred_args) and push @res, $tbl;
-  		  }
-  		  else {
-  		  	&{"PREDICATE_$pred"}($self, $tbl, $pred_args) and push @res, $tbl;
-  		  }
-		  };
-		  if($@ =~ /^Undefined subroutine.*/) {
-		    croak("Unknown predicate $pred");
-		  }
-		  elsif($@) {
-		    croak($@);
-		  }
-	  }
-	}
+  my ($self, @preds) = @_;
+  if((scalar @preds % 2) != 0) {
+    croak("Uneven number of predicates and arguments");
+  }
+  my @res;
+  my $tbl_stat = $self->{dsn}->get_dbh(1)->selectall_arrayref(
+    "SHOW TABLE STATUS FROM `". $$self{dsn}->get('D') ."`",
+    { Slice => {} }
+  );
+  TABLE:
+  foreach my $tbl (@$tbl_stat) {
+    for(my $i=0; $i<$#preds; $i+=2) {
+      no strict 'refs';
+      my $pred = $preds[$i];
+      my @pred_args = (ref($preds[$i+1]) eq 'ARRAY'
+                         ? @{$preds[$i+1]}
+                           : ($preds[$i+1]));
+      my $result = 0;
+      eval {
+
+        $result = &{"PREDICATE_$pred"}($self, $tbl, @pred_args);
+      };
+      if($@ =~ /^Undefined subroutine.*/) {
+        croak("Unknown predicate $pred");
+      }
+      elsif($@) {
+        croak($@);
+      }
+      # Test the predicate result, and if it returns a
+      # false value (0, undef, ''), skip to the next table.
+      next TABLE unless($result);
+    }
+    push @res, $tbl;
+  }
   return @res;
 }
 
@@ -83,13 +90,46 @@ sub PREDICATE_engine {
   return lc($tbl->{Engine}) eq lc($pred_args);
 }
 
+sub PREDICATE_row_format {
+  my ($self, $tbl, $pred_args) = @_;
+  return lc($tbl->{Row_format}) eq lc($pred_args);
+}
+
+sub PREDICATE_agebyname {
+  my ($self, $tbl, $pred_args) = @_;
+  my $tbl_age = undef;
+  my $result  = 0;
+  my $older_than = $$pred_args{'older_than'};
+  my $newer_than = $$pred_args{'newer_than'};
+  my $eq_to      = $$pred_args{'eq_to'};
+  my $fmt = $$pred_args{'pattern'};
+
+  if( not ref($fmt) ) {
+    $fmt = DateTime::Format::Strptime->new(pattern => $fmt,
+                                           time_zone => 'local');
+  }
+  $tbl_age = $fmt->parse_datetime($tbl->{Name});
+  if( $tbl_age ) {
+    if($older_than and DateTime->compare($tbl_age, $older_than) == -1) {
+      return 1;
+    }
+    if($newer_than and DateTime->compare($tbl_age, $newer_than) == 1) {
+      return 1;
+    }
+    if($eq_to and DateTime->compare($tbl_age, $eq_to) == 0) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
 1;
 
 =pod
 
 =head1 NAME
 
-TableFind - Find tables matching a pattern.
+TableFind - Find tables matching a set of patterns.
 
 =head1 SYNOPSIS
 
@@ -100,5 +140,17 @@ TableFind finds tables matching a variety of patterns.
   
   # Find all tables matching /some.*name_(\d+)/
   $tf->find(name => qr/some.*name_(\d+)/);
+
+  # Find all tables newer than 2010-11-01
+  $tf->find(agebyname => { pattern => 'sometbl_%Y%m%d',
+                           newer_than => DateTime->new(year => 2010,
+                                                       month => 11,
+                                                       day => 1)
+                         });
+  # Find all tables starting with somename_ using InnoDB.
+  $tf->find(name => qr/^somename_/, engine => 'innodb');
+
+TableFind will only return the set of tables that match all predicates.
+i.e., the conditions are ANDed together.
 
 =cut
