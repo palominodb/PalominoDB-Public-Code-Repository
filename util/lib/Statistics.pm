1aeb13a2909277f39c582985ba56e2c0050380d1
brian@palominodb.com <brian>
2011-02-10 22:30:17 +0000

    Add mean, stddev, and stdvar calculations to the Statistics module.

--- util/lib/Statistics.pm
+++ util/lib/Statistics.pm
@@ -1,20 +1,20 @@
 # Copyright (c) 2009-2010, PalominoDB, Inc.
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 #   * Redistributions of source code must retain the above copyright notice,
 #     this list of conditions and the following disclaimer.
-# 
+#
 #   * Redistributions in binary form must reproduce the above copyright notice,
 #     this list of conditions and the following disclaimer in the documentation
 #     and/or other materials provided with the distribution.
-# 
+#
 #   * Neither the name of PalominoDB, Inc. nor the names of its contributors
 #     may be used to endorse or promote products derived from this software
 #     without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -29,6 +29,7 @@
 package Statistics;
 use strict;
 use warnings FATAL => 'all';
+use List::Util qw(sum min max reduce);
 use Carp;
 
 sub aggsum {
@@ -48,6 +49,25 @@ sub aggsum {
   return $vals;
 }
 
+sub mean {
+  my ($rows) = @_;
+  $_ = sum(0, @$rows);
+  $_ /= scalar(@$rows);
+  return $_;
+}
+
+sub stdvar {
+  my ($rows) = @_;
+  my $m = mean($rows);
+  my $s = scalar(@$rows);
+  $_ = reduce { $a + ($b - $m)**2 } (0, @$rows);
+  return 1/($s-1)*$_;
+}
+
+sub stddev {
+  return sqrt(stdvar(@_));
+}
+
 =pod
 
 =head1 NAME
@@ -60,6 +80,15 @@ A collection of simple subs to calculate common statistics for reports.
 
   # aggregate $arrayref by col1 and col2
   Statistics::aggsum($arrayref, 'col1', 'col2')
+
+  # calculate the mean of a list of numbers
+  Statistics::mean([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+  # Standard Variance
+  Statistics::stdvar([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+  # Standard Deviation
+  Statistics::stddev([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
 =head1 METHODS
 
@@ -87,6 +116,18 @@ Takes an arrayref of hashrefs and produces a hashref.
          'fred' => { 125 => 1 },
          'bob' =>  { 123 => 1 }
        }
+
+=item C<mean($list)>
+
+Calculates the mean. The average. Exactly what you'd expect.
+
+=item C<stdvar($list)>
+
+The standard variance. See Wikipedia.
+
+=item C<stddev($list)>
+
+The standard deviation. Again, Wikipedia.
 
 =back
 
