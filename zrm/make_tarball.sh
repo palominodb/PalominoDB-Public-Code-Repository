07b9d9ce75c8b606ba72ddd8ee3dee035d4c8ea8
brian@palominodb.com <brian>
2011-02-17 02:28:41 +0000

    Should be able to make a debian package now.

--- zrm/make_tarball.sh
+++ zrm/make_tarball.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # Makes a tarball suitable for passing to 'rpmbuild -tb <tarball>'
+# And hopefully good enough to make a debian package out of, too.
 #
 
 version=$(cat zrm-innobackupex.spec | grep -E '^Version' | awk '{ print $2 }')
@@ -10,6 +11,6 @@ rm -rf zrm-innobackupex-$version.tar.gz
 
 mkdir zrm-innobackupex-$version
 
-rsync -aP examples plugins CHANGELOG README zrm-innobackupex.spec zrm-innobackupex-$version/
+rsync -aP debian examples plugins Makefile CHANGELOG README zrm-innobackupex.spec zrm-innobackupex-$version/
 
 tar czvf zrm-innobackupex-$version.tar.gz zrm-innobackupex-$version/
