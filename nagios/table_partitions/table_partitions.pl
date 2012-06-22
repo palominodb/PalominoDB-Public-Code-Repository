d3c18953f4b33a8c3c1cd6618ac4b27bf224978f
Moss Gross <moss@palominodb.com>
2012-06-22 14:31:03 -0700

    Added GPL header

--- nagios/table_partitions/table_partitions.pl
+++ nagios/table_partitions/table_partitions.pl
@@ -1,4 +1,17 @@
 #!/usr/bin/env perl
+# This program is copyright (c) 2012 PalominoDB. 
+# 
+# THIS PROGRAM IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED 
+# WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF 
+# MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE. 
+# 
+# This program is free software; you can redistribute it and/or modify it under 
+# the terms of the GNU General Public License as published by the Free Software 
+# Foundation, version 2. 
+# 
+# You should have received a copy of the GNU General Public License along with 
+# this program; if not, write to the Free Software Foundation, Inc., 59 Temple 
+# Place, Suite 330, Boston, MA 02111-1307 USA.
 use strict;
 use warnings;
 
