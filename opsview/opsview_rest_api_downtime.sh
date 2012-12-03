5cf7f526c160e458658cd9fc9a8aa5bb50faf343
David Edwards <davidwords@gmail.com>
2012-12-03 13:51:15 -0500

    - added CHANGELOG (output of `git log`) - added COPYING per   https://www.gnu.org/licenses/old-licenses/gpl-2.0.html#SEC4 - added GPLv2 header to opsview_rest_api_downtime.sh

--- opsview/opsview_rest_api_downtime.sh
+++ opsview/opsview_rest_api_downtime.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 #
+# opsview_rest_api_downtime.sh
 # create or delete downtime for a single host using opsview curl rest api
+#
+# Copyright 2012 PalominoDB, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 CURL=/usr/bin/curl
 OPSVIEW_HOSTNAME="opsview.example.com" # HOSTNAME for opsview admin interface
