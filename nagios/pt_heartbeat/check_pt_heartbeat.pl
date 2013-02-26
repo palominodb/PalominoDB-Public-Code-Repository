dc19e6ffd0c668833e264b4d415cc2d53cb72dca
Elmer Medez <EMedez@palominodb.com>
2013-02-27 00:10:44 +0800

    Modified regex pattern to allow decimal point.

--- nagios/pt_heartbeat/check_pt_heartbeat.pl
+++ nagios/pt_heartbeat/check_pt_heartbeat.pl
@@ -1,19 +1,19 @@
 #!/usr/bin/env perl
 # check_mysql_extra monitors replication delay on a slave.
 # Copyright (C) 2012  PalominoDB, Inc.
-# 
+#
 # You may contact the maintainers at eng@palominodb.com.
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
@@ -113,7 +113,7 @@ eval {
   close($out_fh);
   waitpid($pid, 0);
 
-  if($rstr =~ /^(-?\d+)s?$/) {
+  if($rstr =~ /^(-?\d+\.?(\d+)?)s?$/) {
     $rint = $1;
   }
   else {
